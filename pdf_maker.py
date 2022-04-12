3a2115ef8489af3b3f8e4f425a6c00b3ca06a7a7
budmcq2 <58533078+budmcq2@users.noreply.github.com>
2022-04-12 11:48:01 -0500

    Add files via upload

--- pdf_maker.py
+++ pdf_maker.py
@@ -6,3 +6,10 @@ import plotly.express as px
 
 
 twitter_vis = dash.Dash()
+#can add html components to twitter_vis
+
+#Sample, but can use this for most common sentiment 
+df = px.data.gapminder().query("year == 2007").query("continent == 'Europe'")
+df.loc[df['pop'] < 2.e6, 'country'] = 'Other countries' # Represent only large countries
+fig = px.pie(df, values='pop', names='country', title='Population of European continent')
+fig.show()
