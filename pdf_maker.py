9be48ec129b1ed825bf3098f0f59376dbe48c57a
budmcq2 <58533078+budmcq2@users.noreply.github.com>
2022-04-12 11:43:22 -0500

    Add files via upload

--- pdf_maker.py
+++ pdf_maker.py
@@ -3,3 +3,6 @@ import dash_html_components as html #if we want to include html components
 import dash_core_components as dcc
 import plotly.graph_objects as go
 import plotly.express as px
+
+
+twitter_vis = dash.Dash()
