d68cb6bb1fd6665e377ff0128c8cd04a60eabb13
budmcq2 <58533078+budmcq2@users.noreply.github.com>
2022-04-28 11:56:05 -0500

    Add loyal follower

--- dash_test/app.py
+++ dash_test/app.py
@@ -2,17 +2,32 @@ from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
 import dash
 
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+loyalFollowerDate = "01/01/2000"
+loyalFollowerHandle = "@TWITTER"
+loyalFollowerLikes = 0
+
+lfStr = str("Your most loyal follower: " + loyalFollowerHandle)
+lfDateStr = str(loyalFollowerHandle + " has followed you since " + loyalFollowerDate + ".")
+lfLikeStr = str(loyalFollowerHandle + " has liked " + str(loyalFollowerLikes) + " of your tweets.")
 
 app.layout = html.Div([
     html.P('Enter your twitter handle'),
     # debounce making sure enter is pressed
     dcc.Input(id='handle', type='text', debounce=True),
     html.P(id='err', style={'color': 'red'}),
-    html.P(id='out')
+    html.P(id='out'),
+    html.Div([
+        html.H2(lfStr),
+        html.P(lfDateStr),
+        html.P(lfLikeStr)
+    ])
 ])
+
 
 @app.callback(
     Output('out', 'children'),
