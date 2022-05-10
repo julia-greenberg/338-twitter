c031f2cbf8103c42b2e0f3baf2f1071dfbd747cc
armanpopli2023 <armanpopli2023@u.northwestern.edu>
2022-05-10 10:39:48 -0500

    incorrect page

--- dash_test/app.py
+++ dash_test/app.py
@@ -1,12 +1,18 @@
+
 from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
 import dash
 import json
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# MY STUFF ADDED HERE - sentiment: score between 0 and 1
+sentiment = 0.55
+sentiment_strings = helpers.sentiment_score(sentiment)
+sentiment_percent = helpers.sentiment_breakdown(sentiment)
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+times = json_data["times"]
+
+user = json_data["user"]
 
 json_file = open("sample_comm.json")
 json_data = json.load(json_file)
@@ -76,6 +82,70 @@ def show_handle(handle):
     # pass to the back end
 
     return 'Your Twitter handle is @{}.'.format(handle), ''
+
+
+
+page_2_layout = html.Div([
+    html.Header([
+        html.H1(f'Welcome to TwitterHawk, @{user}'),
+        DashIconify(icon="fa-solid:fa-bars")
+    ]),
+    html.Aside([
+        html.Div(
+            className="row",
+            children = [
+            html.Section([
+                html.H2(lfStr),
+                html.P(lfDateStr),
+                html.P(lfLikeStr),
+                ]),
+            html.Section([
+                html.H2("Your most popular topics:"),
+                html.Ol(
+                    [
+                        html.Li(str(int_list_data[0][0] + ": " + str(int_list_data[0][1]))),
+                        html.Li(str(int_list_data[1][0] + ": " + str(int_list_data[1][1]))),
+                        html.Li(str(int_list_data[2][0] + ": " + str(int_list_data[2][1])))
+                    ]
+                )
+            ]),
+            html.Section([
+                html.H2("Your most popular tweet:"),
+                html.Iframe(srcDoc = topTweetStr, height = 300, width = 400)
+            ]),
+        ]),
+        html.Div(
+            className="row2",
+            children = [
+            html.Section([
+                    html.H2("Your least popular tweet:"),
+                    html.Iframe(srcDoc = leastTweetStr, height = 300, width = 400)
+            ]),
+            html.Section([
+                html.H2("On twitter, you tend to be " + sentiment_strings[0]),
+                html.P(sentiment_strings[1]),
+                dcc.Graph(id="graph", figure=helpers.generate_chart(sentiment, user))
+                ]),
+            html.Section([
+                html.H2("You're most active on Twitter during..."),
+                dcc.Graph(
+                    figure={
+                        'data': [
+                            {'x': list(times.keys()), 'y': list(times.values()), 'type': 'bar', 'name': 'Times'}
+                        ],
+                        'layout': {
+                            'title': 'Times you Tweet',
+                            'figure.layout.autosize': True,
+                            # 'config.responsive': True,
+                            'figure.layout.height': '300px'
+                        }
+                    }
+                )      
+            ])])
+    ]),
+    html.P(id='err', style={'color': 'red'}),
+    html.P(id='out')
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
