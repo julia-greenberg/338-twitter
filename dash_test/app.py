e134b73ac9d99c51478ffb6b2e28dbc85012ea1a
armanpopli2023 <armanpopli2023@u.northwestern.edu>
2022-05-10 10:47:44 -0500

    close not sure why

--- dash_test/app.py
+++ dash_test/app.py
@@ -1,18 +1,18 @@
-
 from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
 import dash
+from dash_iconify import DashIconify
 import json
 
+import helpers
+import plotly.express as px
+import plotly.graph_objects as go
 
-# MY STUFF ADDED HERE - sentiment: score between 0 and 1
-sentiment = 0.55
-sentiment_strings = helpers.sentiment_score(sentiment)
-sentiment_percent = helpers.sentiment_breakdown(sentiment)
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap']
 
-times = json_data["times"]
-
-user = json_data["user"]
+app = Dash(__name__, title="TwitterHawk", external_stylesheets=external_stylesheets)
+app.css.config.serve_locally = True
 
 json_file = open("sample_comm.json")
 json_data = json.load(json_file)
@@ -31,42 +31,34 @@ lfStr = str("Your most loyal follower: " + loyalFollowerHandle)
 lfDateStr = str(loyalFollowerHandle + " has followed you since " + loyalFollowerDate + ".")
 lfLikeStr = str(loyalFollowerHandle + " has liked " + str(loyalFollowerLikes) + " of your tweets.")
 
-#htmlStr =  '<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Sunsets don&#39;t get much better than this one over <a href="https://twitter.com/GrandTetonNPS?ref_src=twsrc%5Etfw">@GrandTetonNPS</a>. <a href="https://twitter.com/hashtag/nature?src=hash&amp;ref_src=twsrc%5Etfw">#nature</a> <a href="https://twitter.com/hashtag/sunset?src=hash&amp;ref_src=twsrc%5Etfw">#sunset</a> <a href="http://t.co/YuKy2rcjyU">pic.twitter.com/YuKy2rcjyU</a></p>&mdash; US Department of the Interior (@Interior) <a href="https://twitter.com/Interior/status/463440424141459456?ref_src=twsrc%5Etfw">May 5, 2014</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>"'
 topTweetStr = '<blockquote class="twitter-tweet"><a href="' + json_data["most popular tweet"] + '"></a></blockquote><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
 leastTweetStr = '<blockquote class="twitter-tweet"><a href="' + json_data["least popular tweet"] + '"></a></blockquote><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
 
+# MY STUFF ADDED HERE - sentiment: score between 0 and 1
+sentiment = 0.55
+sentiment_strings = helpers.sentiment_score(sentiment)
+sentiment_percent = helpers.sentiment_breakdown(sentiment)
+
+times = json_data["times"]
+
+user = json_data["user"]
+# -------
+
 app.layout = html.Div([
+    html.Div('Welcome to TwitterHawk', style={'color': 'blue', 'background-color': 'grey', 'height': 60, 'fontSize': 20, 'text-align': 'center'}),
     html.P('Enter your twitter handle'),
     # debounce making sure enter is pressed
-    dcc.Input(id='handle', type='text', debounce=True),
+    html.Div([
+        html.P('@'),
+        dcc.Input(id='handle', type='text', debounce=True)
+        ], style={'display': 'flex', 'text-align': 'justify'}),
     html.P(id='err', style={'color': 'red'}),
     html.P(id='out'),
     html.Div([
-        html.H2(lfStr),
-        html.P(lfDateStr),
-        html.P(lfLikeStr)
-    ]),
-    html.Div([
-        html.Ol(
-            [
-                html.Li(str(int_list_data[0][0] + ": " + str(int_list_data[0][1]))),
-                html.Li(str(int_list_data[1][0] + ": " + str(int_list_data[1][1]))),
-                html.Li(str(int_list_data[2][0] + ": " + str(int_list_data[2][1])))
-            ]
-        )
-    ]),
-    #<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Sunsets don&#39;t get much better than this one over <a href="https://twitter.com/GrandTetonNPS?ref_src=twsrc%5Etfw">@GrandTetonNPS</a>. <a href="https://twitter.com/hashtag/nature?src=hash&amp;ref_src=twsrc%5Etfw">#nature</a> <a href="https://twitter.com/hashtag/sunset?src=hash&amp;ref_src=twsrc%5Etfw">#sunset</a> <a href="http://t.co/YuKy2rcjyU">pic.twitter.com/YuKy2rcjyU</a></p>&mdash; US Department of the Interior (@Interior) <a href="https://twitter.com/Interior/status/463440424141459456?ref_src=twsrc%5Etfw">May 5, 2014</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-    html.Div([
-        html.H2("Your most popular tweet:"),
-        html.Iframe(srcDoc = topTweetStr, height = 300, width = 400)
-    ]),
-    html.Div([
-        html.H2("Your least popular tweet:"),
-        html.Iframe(srcDoc = leastTweetStr, height = 300, width = 400)
+        dcc.Link('Analyze', href='/page-2'),
+
     ])
-
 ])
-
 
 @app.callback(
     Output('out', 'children'),
@@ -82,8 +74,6 @@ def show_handle(handle):
     # pass to the back end
 
     return 'Your Twitter handle is @{}.'.format(handle), ''
-
-
 
 page_2_layout = html.Div([
     html.Header([
@@ -146,6 +136,9 @@ page_2_layout = html.Div([
     html.P(id='err', style={'color': 'red'}),
     html.P(id='out')
 ])
+
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
