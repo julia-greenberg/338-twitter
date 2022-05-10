22ced7c0d2c1f7e4a672bc25c4f7529adfeed103
Julia Greenberg <juliagreenberg2023@u.northwestern.edu>
2022-05-10 17:20:25 -0500

    submit button working sucessfully

--- dash_test/app.py
+++ dash_test/app.py
@@ -17,187 +17,140 @@ app.css.config.serve_locally = True
 json_file = open("sample_comm.json")
 json_data = json.load(json_file)
 
-loyalFollowerDate = "01/01/2000"
-loyalFollowerHandle = "@" + str(json_data["longest follower"])
-loyalFollowerLikes = 0
-
-interest_json = json_data["interests"]
-top_interests = list(interest_json.keys())
-int_list_data = []
-for i in range(len(top_interests)):
-    int_list_data.append((top_interests[i],interest_json[top_interests[i]] ))
-
-lfStr = str("Your most loyal follower: " + loyalFollowerHandle)
-lfDateStr = str(loyalFollowerHandle + " has followed you since " + loyalFollowerDate + ".")
-lfLikeStr = str(loyalFollowerHandle + " has liked " + str(loyalFollowerLikes) + " of your tweets.")
-
-topTweetStr = '<blockquote class="twitter-tweet"><a href="' + json_data["most popular tweet"] + '"></a></blockquote><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
-leastTweetStr = '<blockquote class="twitter-tweet"><a href="' + json_data["least popular tweet"] + '"></a></blockquote><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
-
-# MY STUFF ADDED HERE - sentiment: score between 0 and 1
-sentiment = 0.55
-sentiment_strings = helpers.sentiment_score(sentiment)
-sentiment_percent = helpers.sentiment_breakdown(sentiment)
-
-times = json_data["times"]
-
-user = json_data["user"]
-# -------
-
-# app.layout = html.Div([
-#     dcc.Location(id='url', refresh=False),
-#     html.Div(id='page-content'),
-#     dcc.Link(html.Button(id='submit-button', type='submit', children='Submit'), href='/page-2', style={'display':'none'}),
-#     dcc.Input(id='handle', type='text', debounce=True, style={'display': 'none'})
-# ])
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div(id='page-content'),
-    html.Header([
-        html.H1(f'Welcome to TwitterHawk')]),
-    html.Aside([
-        html.Div(
-            className='home',
-            children=[
-                html.P('Enter your twitter handle'),
-                html.Div([
-                    html.P('@'),
-                    dcc.Input(id='handle', type='text', debounce=True)
-                ], style={'display': 'flex', 'text-align': 'justify'}),
-                # html.P(id='err', style={'color': 'red'}),
-                # html.P(id='out'),
-                # html.Div([
-                #     dcc.Link('Analyze', href='/page-2'),
-                # ])
-                dcc.Link(html.Button(id='submit-button', type='submit', children='Submit'), href='/page-2')
+    html.Div(
+        id="home-page",
+        children = [
+            html.Header([
+                html.H1(f'Welcome to TwitterHawk')]),
+            html.Aside([
+                html.Div(
+                    className='home',
+                    children=[
+                        html.P('Enter your twitter handle'),
+                        html.Div([
+                            html.P('@'),
+                            dcc.Input(id='handle', type='text', debounce=True)
+                        ], style={'display': 'flex', 'text-align': 'justify'}),
+                        dcc.Link(html.Button(id='submit-button', type='submit', children='Submit'), href='/page-2')
+                    ])
+            # debounce making sure enter is pressed
+
             ])
-    # debounce making sure enter is pressed
-
-    ])
-])
-
-# @app.callback(Output('page-content', 'children'),
-#               [Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/page-2':
-        print("called")
-        return page_2_layout
-    else:
-        print("here")
-        return input_page_
-
-input_page_ = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content'),
-    html.Header([
-        html.H1(f'Welcome to TwitterHawk')]),
-    html.Aside([
-        html.Div(
-            className='home',
-            children=[
-                html.P('Enter your twitter handle'),
-                html.Div([
-                    html.P('@'),
-                    dcc.Input(id='handle', type='text', debounce=True)
-                ], style={'display': 'flex', 'text-align': 'justify'}),
-                # html.P(id='err', style={'color': 'red'}),
-                # html.P(id='out'),
-                # html.Div([
-                #     dcc.Link('Analyze', href='/page-2'),
-                # ])
-                dcc.Link(html.Button(id='submit-button', type='submit', children='Submit'), href='/page-2', id='submit-button')
-            ])
-    # debounce making sure enter is pressed
-
-    ])
+        ]
+    ),
 ])
 
 @app.callback(
     # Output('out', 'children'),
     # Output('err', 'children'),
+    Output('home-page','children'),
     Output('page-content', 'children'),
     [Input('submit-button', 'n_clicks')],
-    [Input('url', 'pathname')],
-    # Input('handle', 'value'),
-    [Input('handle', 'value')]
+    [State('url', 'pathname')],
+    [State('handle', 'value')]
 )
-
 def show_handle(clicks, pathname, handle):
     if handle is None:
         # PreventUpdate prevents ALL outputs updating
         raise dash.exceptions.PreventUpdate
     if clicks is not None and handle is not None:
-        # print(handle)
         if pathname == "/page-2":
-            return page_2_layout
-        # return 'Your Twitter handle is @{}.'.format(handle), ''
-    # do something with twitter handle once its submitted?
-    # pass to the back end
+            # some call to access/analysis which creates "json data"
+            return [[], user_info(json_data, handle)]
 
-    # return 'Your Twitter handle is @{}.'.format(handle), ''
+def user_info(json_data, username):
+    loyalFollowerDate = "01/01/2000"
+    loyalFollowerHandle = "@" + str(json_data["longest follower"])
+    loyalFollowerLikes = 0
 
-page_2_layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Header([
-        html.H1(f'Welcome to TwitterHawk, @{user}'),
-        DashIconify(icon="fa-solid:fa-bars")
-    ]),
-    html.Aside([
-        html.Div(
-            className="row",
-            children = [
-            html.Section([
-                html.H2(lfStr),
-                html.P(lfDateStr),
-                html.P(lfLikeStr),
-                ]),
-            html.Section([
-                html.H2("Your most popular topics:"),
-                html.Ol(
-                    [
-                        html.Li(str(int_list_data[0][0] + ": " + str(int_list_data[0][1]))),
-                        html.Li(str(int_list_data[1][0] + ": " + str(int_list_data[1][1]))),
-                        html.Li(str(int_list_data[2][0] + ": " + str(int_list_data[2][1])))
-                    ]
-                )
-            ]),
-            html.Section([
-                html.H2("Your most popular tweet:"),
-                html.Iframe(srcDoc = topTweetStr, height = 300, width = 400)
-            ]),
+    interest_json = json_data["interests"]
+    top_interests = list(interest_json.keys())
+    int_list_data = []
+    for i in range(len(top_interests)):
+        int_list_data.append((top_interests[i],interest_json[top_interests[i]] ))
+
+    lfStr = str("Your most loyal follower: " + loyalFollowerHandle)
+    lfDateStr = str(loyalFollowerHandle + " has followed you since " + loyalFollowerDate + ".")
+    lfLikeStr = str(loyalFollowerHandle + " has liked " + str(loyalFollowerLikes) + " of your tweets.")
+
+    topTweetStr = '<blockquote class="twitter-tweet"><a href="' + json_data["most popular tweet"] + '"></a></blockquote><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
+    leastTweetStr = '<blockquote class="twitter-tweet"><a href="' + json_data["least popular tweet"] + '"></a></blockquote><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
+
+    # MY STUFF ADDED HERE - sentiment: score between 0 and 1
+    sentiment = 0.55
+    sentiment_strings = helpers.sentiment_score(sentiment)
+    sentiment_percent = helpers.sentiment_breakdown(sentiment)
+
+    times = json_data["times"]
+
+    user = json_data["user"]
+
+    page_2_layout = html.Div([
+        dcc.Location(id='url', refresh=False),
+        html.Header([
+            html.H1(f'Welcome to TwitterHawk, @{username}'),
+            DashIconify(icon="fa-solid:fa-bars")
         ]),
-        html.Div(
-            className="row2",
-            children = [
-            html.Section([
-                    html.H2("Your least popular tweet:"),
-                    html.Iframe(srcDoc = leastTweetStr, height = 300, width = 400)
-            ]),
-            html.Section([
-                html.H2("On twitter, you tend to be " + sentiment_strings[0]),
-                html.P(sentiment_strings[1]),
-                dcc.Graph(id="graph", figure=helpers.generate_chart(sentiment, user))
+        html.Aside([
+            html.Div(
+                className="row",
+                children = [
+                html.Section([
+                    html.H2(lfStr),
+                    html.P(lfDateStr),
+                    html.P(lfLikeStr),
+                    ]),
+                html.Section([
+                    html.H2("Your most popular topics:"),
+                    html.Ol(
+                        [
+                            html.Li(str(int_list_data[0][0] + ": " + str(int_list_data[0][1]))),
+                            html.Li(str(int_list_data[1][0] + ": " + str(int_list_data[1][1]))),
+                            html.Li(str(int_list_data[2][0] + ": " + str(int_list_data[2][1])))
+                        ]
+                    )
                 ]),
-            html.Section([
-                html.H2("You're most active on Twitter during..."),
-                dcc.Graph(
-                    figure={
-                        'data': [
-                            {'x': list(times.keys()), 'y': list(times.values()), 'type': 'bar', 'name': 'Times'}
-                        ],
-                        'layout': {
-                            'title': 'Times you Tweet',
-                            'figure.layout.autosize': True,
-                            # 'config.responsive': True,
-                            'figure.layout.height': '300px'
+                html.Section([
+                    html.H2("Your most popular tweet:"),
+                    html.Iframe(srcDoc = topTweetStr, height = 300, width = 400)
+                ]),
+            ]),
+            html.Div(
+                className="row2",
+                children = [
+                html.Section([
+                        html.H2("Your least popular tweet:"),
+                        html.Iframe(srcDoc = leastTweetStr, height = 300, width = 400)
+                ]),
+                html.Section([
+                    html.H2("On twitter, you tend to be " + sentiment_strings[0]),
+                    html.P(sentiment_strings[1]),
+                    dcc.Graph(id="graph", figure=helpers.generate_chart(sentiment, user))
+                    ]),
+                html.Section([
+                    html.H2("You're most active on Twitter during..."),
+                    dcc.Graph(
+                        figure={
+                            'data': [
+                                {'x': list(times.keys()), 'y': list(times.values()), 'type': 'bar', 'name': 'Times'}
+                            ],
+                            'layout': {
+                                'title': 'Times you Tweet',
+                                'figure.layout.autosize': True,
+                                # 'config.responsive': True,
+                                'figure.layout.height': '300px'
+                            }
                         }
-                    }
-                )      
-            ])])
-    ]),
-    html.P(id='err', style={'color': 'red'}),
-    html.P(id='out')
-])
+                    )      
+                ])])
+        ]),
+        html.P(id='err', style={'color': 'red'}),
+        html.P(id='out')
+    ])
+    return page_2_layout
 
 
 
